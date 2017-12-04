390a2d3ecceaacde3a36637f39ce235040008872
quantumandroid <quantumandroid@users.noreply.github.com>
2017-12-04 16:16:29 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -230,3 +230,198 @@ menu.main.xml
         app:showAsAction="ifRoom"
         />
 </menu>
+
+
+
+/////////////
+// DBHelper.java
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "MyDatabase.db";
+
+    // Table name
+    private static final String TABLE_NAME1 = "tb1";
+
+    // Table Columns names
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_CITY = "place";
+    public static final String COLUMN_PHONE = "phone";
+
+    //Create Table's
+    public final String CREATE_TABLE1 = "create table " + TABLE_NAME1 +
+            "(id integer primary key, name text,phone text,place text)";
+
+    // Default constructor
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Creating Tables
+        sqLiteDatabase.execSQL(CREATE_TABLE1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Drop older table's if existed
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME1);
+
+        // Create tables again
+        onCreate(sqLiteDatabase);
+
+    }
+
+    // Select Query (TODO select specific row with all columns)
+    public Cursor select(String TABLE_NAME, String COLUMN_NAME, String COLUMN_VALUE) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " where " + COLUMN_NAME + "=" + COLUMN_VALUE, null);
+        return res;
+    }
+
+
+    // Select Query (TODO select specific row with specific column)
+    public String select(String TABLE_NAME, String SELECT_COLUMN_NAME, String WHERE_COLUMN_NAME, String WHERE_COLUMN_VALUE) {
+        String str = "";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select " + SELECT_COLUMN_NAME + " from " + TABLE_NAME + " where " + WHERE_COLUMN_NAME + "=" + WHERE_COLUMN_VALUE, null);
+        str = res.getString(res.getColumnIndex(SELECT_COLUMN_NAME));
+
+
+        res.close();
+        db.close();
+        return str;
+    }
+
+
+    // Select Query (TODO select single column with all rows)
+    public ArrayList<String> select(String TABLE_NAME, String SELECT_COLUMN_NAME) {
+
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from" + TABLE_NAME, null);
+        res.moveToFirst();
+
+        while (res.isAfterLast() == false) {
+            array_list.add(res.getString(res.getColumnIndex(SELECT_COLUMN_NAME)));
+            res.moveToNext();
+        }
+
+
+        res.close();
+        db.close();
+        return array_list;
+    }
+
+
+    // Select Query (TODO select (*) all columns with all rows  )
+    public Cursor select(String TABLE_NAME) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        return db.rawQuery("select * from" + TABLE_NAME, null);
+    }
+
+
+    //Insert Query
+    public boolean insert(String TABLE_NAME, ContentValues contentValues) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(TABLE_NAME, null, contentValues);
+
+        db.close();
+        return true;
+    }
+
+
+    // Update Query
+    public boolean update(String TABLE_NAME, ContentValues contentValues, String COLUMN_NAME, String COLUMN_VALUE) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.update(TABLE_NAME, contentValues, COLUMN_NAME + " = ? ", new String[]{COLUMN_VALUE});
+
+        db.close();
+
+        return true;
+    }
+
+    //Delete Query
+    public Integer delete(String TABLE_NAME, String COLUMN_NAME, String COLUMN_VALUE) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, COLUMN_NAME + " = ? ", new String[]{COLUMN_VALUE});
+    }
+
+    // check if a record exists so it won't insert the next time you run this code
+    public boolean ifExists(String TABLE_NAME, String CHECK_COLUMN_NAME, String CHECK_COLUMN_VALUE) {
+
+        boolean recordExists = false;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT " + CHECK_COLUMN_NAME + " FROM " + TABLE_NAME + " WHERE " + CHECK_COLUMN_NAME + " = '" + CHECK_COLUMN_VALUE + "'", null);
+
+        if (cursor != null) {
+
+            if (cursor.getCount() > 0) {
+                recordExists = true;
+            }
+        }
+
+
+        if (cursor != null) {
+            cursor.close();
+        }
+        db.close();
+
+        return recordExists;
+    }
+
+
+}
+
+
+/*
+TODO    Save data in ContentValues
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, "name");
+        contentValues.put(COLUMN_PHONE, phone);
+*/
+
+/*
+TODO    Get data from Cursor
+        //SQLiteDatabase db = this.getReadableDatabase();
+        //Cursor cursor = db.rawQuery("select * from" + TABLE_NAME, null);
+
+        cursor.moveToFirst();
+        while (cursor.isAfterLast() == false) {
+            String str = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
+            int id = cursor.getInt(cursor.getColumnIndex(COLUMN_ID));
+            cursor.moveToNext();
+        }
+*/
+
+
+/*   String sql = "";
+
+        sql += "CREATE TABLE " + tableName;
+        sql += " ( ";
+        sql += fieldObjectId + " INTEGER PRIMARY KEY AUTOINCREMENT, ";
+        sql += fieldObjectName + " TEXT ";
+        sql += " ) ";
+
+        db.execSQL(sql);*/
+
+
+
